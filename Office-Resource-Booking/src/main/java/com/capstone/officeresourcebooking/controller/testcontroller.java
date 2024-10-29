90d3718e10e8b497f4a1b9ecf5ac05b8e04b78cd
Botjax <Frozeneagle1607@gmail.com>
2024-10-29 14:21:50 -0500

    refactoring

--- Office-Resource-Booking/src/main/java/com/capstone/officeresourcebooking/controller/testcontroller.java
+++ Office-Resource-Booking/src/main/java/com/capstone/officeresourcebooking/controller/testcontroller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class testcontroller {
+public class TestController {
     @GetMapping("/helloworld")
     public String helloworld() {
         return "Hello World";
